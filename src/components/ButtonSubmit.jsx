e026b48aa9a48a6a41d4222b496f158b31a28976
CTSDM <156813428+CTSDM@users.noreply.github.com>
2025-02-06 20:12:35 +0900

    Add test for component

--- src/components/ButtonSubmit.jsx
+++ src/components/ButtonSubmit.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import styles from "./ButtonSubmit.module.css";
 
-export default function ButtonSubmit({ text }) {
+export default function ButtonSubmit({ text = "Submit" }) {
     return (
         <button type="submit" className={styles.input}>
             {text}
---
+++ tests/ButtonSubmit.test.jsx
@@ -0,0 +1,24 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import ButtonSubmit from "../src/components/ButtonSubmit.jsx";
+
+describe("The component ", () => {
+    it("should be rendered", () => {
+        render(<ButtonSubmit text={""} />);
+        const button = screen.getByRole("button");
+        expect(button).toBeInTheDocument();
+    });
+
+    it("should be rendered with a given text", () => {
+        const text = "Random text";
+        render(<ButtonSubmit text={text} />);
+        const button = screen.getByRole("button");
+        expect(button).toHaveTextContent(text);
+    });
+
+    it("should be rendered with a fallback text if no text is given", () => {
+        render(<ButtonSubmit />);
+        const button = screen.getByRole("button");
+        expect(button).toHaveTextContent("Submit");
+    });
+});
